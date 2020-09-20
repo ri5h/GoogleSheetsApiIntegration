f87f2b22168cf31c7bc7c37bb49732ba62749726
Rishiraj Purohit <rishiraj.purohit@internations.org>
2020-09-20 22:02:19 +0200

    ChkPoint Restructure and refactor

---
+++ public/src/sheet-loader.php
@@ -0,0 +1,128 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('Google Sheets API Access');
+    $client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
+    $client->setAuthConfig('../credentials.json');
+    $client->setAccessType('offline');
+    $client->setPrompt('select_account consent');
+
+    // Load previously authorized token from a file, if it exists.
+    // The file token.json stores the user's access and refresh tokens, and is
+    // created automatically when the authorization flow completes for the first
+    // time.
+    $tokenPath = '../token.json';
+    if (file_exists($tokenPath)) {
+        $accessToken = json_decode(file_get_contents($tokenPath), true);
+        $client->setAccessToken($accessToken);
+    }
+
+    // If there is no previous token or it's expired.
+    if ($client->isAccessTokenExpired()) {
+        // Refresh the token if possible, else fetch a new one.
+        if ($client->getRefreshToken()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        } else {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+            $client->setAccessToken($accessToken);
+
+            // Check to see if there was an error.
+            if (array_key_exists('error', $accessToken)) {
+                throw new Exception(join(', ', $accessToken));
+            }
+        }
+        // Save the token to a file.
+        if (!file_exists(dirname($tokenPath))) {
+            mkdir(dirname($tokenPath), 0700, true);
+        }
+        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
+}
+
+
+function getData()
+{
+
+    $datastr = '{"player":"11","vikings":"12","wildcats":"4","bases":{"position":{"base_1":"1","base_2":"1","base_3":"0"},"first_base":{"ball":"3","strike":"2","out":"1"},"second_base":{"ball":null,"strike":null,"out":null},"third_base":{"ball":null,"strike":null,"out":null}},"inning":{"home":["0","1","0","11"," "," "," "],"visitor":["1","0","2","1"," "," "," "],"total":{"home":"12","visitor":"4"}}}';
+    return json_decode($datastr, true);
+
+    // Get the API client and construct the service object.
+    $client = getClient();
+    $service = new Google_Service_Sheets($client);
+    $data = [];
+
+    $spreadsheetId = '11fClC_NhZ6uchStr9fEOKz9a3qITRrT1rT_9UiX_mA4';
+    $range = 'A1:I14';
+    $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+    $values = $response->getValues();
+
+    $data = [
+        "player" => $values[0][1],
+        "vikings" => $values[1][1],
+        "wildcats" => $values[2][1],
+        "bases" => [
+            "position" => [
+                "base_1" => $values[4][1],
+                "base_2" => $values[4][2],
+                "base_3" => $values[4][3],
+            ],
+            "first_base" => [
+                "ball" => $values[5][1],
+                "strike" => $values[6][1],
+                "out" => $values[7][1],
+            ],
+            "second_base" => [
+                "ball" => $values[5][2],
+                "strike" => $values[6][2],
+                "out" => $values[7][2],
+            ],
+            "third_base" => [
+                "ball" => $values[5][3],
+                "strike" => $values[6][3],
+                "out" => $values[7][3],
+            ]
+        ],
+        "inning" => [
+            "home" => [
+                $values[12][1] ?? "&nbsp;",
+                $values[12][2] ?? "&nbsp;",
+                $values[12][3] ?? "&nbsp;",
+                $values[12][4] ?? "&nbsp;",
+                $values[12][5] ?? "&nbsp;",
+                $values[12][6] ?? "&nbsp;",
+                $values[12][7] ?? "&nbsp;",
+            ],
+            "visitor" => [
+                $values[13][1] ?? "&nbsp;",
+                $values[13][2] ?? "&nbsp;",
+                $values[13][3] ?? "&nbsp;",
+                $values[13][4] ?? "&nbsp;",
+                $values[13][5] ?? "&nbsp;",
+                $values[13][6] ?? "&nbsp;",
+                $values[13][7] ?? "&nbsp;",
+            ],
+            "total" => [
+                "home" => $values[12][8],
+                "visitor" => $values[13][8]
+            ]
+        ]
+    ];
+
+    return $data;
+}
---
+++ public/updated/index.php
@@ -0,0 +1,148 @@
+<?php
+// Make sure we get Sheet Id
+$sheetId = $_GET["sheetId"] ?? exit("Sheet Id is Missing in the Url, please check the link");
+
+// Load data from Sheet
+require_once 'src/sheet-loader.php';
+$data = getData();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Baseball Score Card</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/styles-v1.css" />
+</head>
+
+<body class="container-fluid">
+    <div class="row justify-content-center mt-md-4">
+        <div class="col-sm-11 col-md-10 scorecard p-2">
+            <div class="header text-center text-white py-2">
+                The National Championship
+            </div>
+            <div class="scoreboard-body">
+                <div class="row" id="top-row">
+                    <div class="col">
+                        <div class="col-title">At Bat</div>
+                        <div class="col-body number">8</div>
+                    </div>
+                    <div class="col">
+                        <div class="col-title">Ball</div>
+                        <div class="col-body number">11</div>
+                    </div>
+                    <div class="col">
+                        <div class="col-title">Strike</div>
+                        <div class="col-body number">891</div>
+                    </div>
+                    <div class="col">
+                        <div class="col-title">Out</div>
+                        <div class="col-body number">11</div>
+                    </div>
+                    <div class="col field-column">
+                        <div class="col-title">Field</div>
+                        <div class="col-body">
+                            <!-- prettier-ignore -->
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="134.853 129.453 117.75 92.75">
+                                <rect stroke="#000" id="svg_1" height="92.75" width="117.75" y="129.453" x="134.853" stroke-width="1.5" fill="#000000" />
+                                <g id="svg_5" transform="matrix(1, 0, 0, 1, -206.271652, -124.172058)">
+                                    <path stroke="#000" id="svg_2" d="m342.87499,314.75l25.75,-24.50001l25.75,24.50001l-25.75,24.50001l-25.75,-24.50001z" stroke-width="1.5" fill="#999999" />
+                                    <path stroke="#000" id="svg_3" d="m374.12499,285l25.75,-24.50001l25.75,24.50001l-25.75,24.50001l-25.75,-24.50001z" stroke-width="1.5" fill="#ffff00" />
+                                    <path stroke="#000" id="svg_4" d="m405.62499,315l25.75,-24.50001l25.75,24.50001l-25.75,24.50001l-25.75,-24.50001z" stroke-width="1.5" fill="#999999" />
+                                </g>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-4 row-eq-height" id="bottom-row">
+                    <div class="col-2" id="leftside">
+                        <div class="col-title">Teams</div>
+                        <div class="col-body-number text-white">
+                            <div class="mb-2 innings-text">Home</div>
+                            <div class="innings-text">Visitor</div>
+                        </div>
+                    </div>
+                    <div class="col-8" id="center">
+                        <div class="row">
+                            <div class="col">
+                                <div class="col-title">1</div>
+                                <div class="col-body-number text-danger">
+                                    <div class="mb-2 number-score bg-black text-red">8</div>
+                                    <div class="number-score bg-black text-red">7</div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="col-title">1</div>
+                                <div class="col-body-number text-danger">
+                                    <div class="mb-2 number-score bg-black text-red">0</div>
+                                    <div class="number-score bg-black text-red">7</div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="col-title">1</div>
+                                <div class="col-body-number text-danger">
+                                    <div class="mb-2 number-score bg-black text-red">8</div>
+                                    <div class="number-score bg-black text-red">7</div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="col-title">1</div>
+                                <div class="col-body-number text-danger">
+                                    <div class="mb-2 number-score bg-black text-red">8</div>
+                                    <div class="number-score bg-black text-red">7</div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="col-title">1</div>
+                                <div class="col-body-number text-danger">
+                                    <div class="mb-2 number-score bg-black text-red">8</div>
+                                    <div class="number-score bg-black text-red">7</div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="col-title">1</div>
+                                <div class="col-body-number text-danger">
+                                    <div class="mb-2 number-score bg-black text-red">8</div>
+                                    <div class="number-score bg-black text-red">7</div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="col-title">1</div>
+                                <div class="col-body-number text-danger">
+                                    <div class="mb-2 number-score bg-black text-red">8</div>
+                                    <div class="number-score bg-black text-red">7</div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="col-title">1</div>
+                                <div class="col-body-number text-danger">
+                                    <div class="mb-2 number-score bg-black text-red">8</div>
+                                    <div class="number-score bg-black text-red">7</div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="col-title">1</div>
+                                <div class="col-body-number text-danger">
+                                    <div class="mb-2 number-score bg-black text-red">8</div>
+                                    <div class="number-score bg-black text-red">7</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-2" id="right">
+                        <div class="col-title">Total</div>
+                        <div class="col-body-number" style="margin: 0 auto; width: 90%">
+                            <div class="mb-2 number bg-black text-red">8</div>
+                            <div class="number bg-black text-red">7</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
